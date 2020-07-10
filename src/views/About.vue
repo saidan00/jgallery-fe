5ca49c7fc25bc7f4d163a4fb53a7982f9d8f2e3c
JaySGH <huysgh94@gmail.com>
2020-07-10 22:17:53 +0700

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="row justify-content-center">
     <div class="col-md-6 pt-4" data-aos="fade-up">
-      <h2 class="text-white mb-4">About Me</h2>
+      <h2 class="text-white mb-4">Hey There! I'm JaySGH</h2>
 
       <div class="row">
         <div class="col-md-12">
-          <p
-            class="mb-5"
-          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa explicabo quasi cum, laudantium neque at veniam itaque atque necessitatibus temporibus! Beatae sit soluta magni neque autem, suscipit dolorem, quo alias.</p>
+          <p>This is a gallery I made for keeping photos, images, pictures, ... that I collected from the whole internet, or taken by me.</p>
+          <p class="mb-5">
+            For more information, please contact:
+            <a
+              href="https://www.facebook.com/huy.sgh"
+              target="_blank"
+            >my facebook</a>
+          </p>
         </div>
       </div>
     </div>
